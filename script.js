c876dd1a588d9364f5e4063c42cf1e276e7d436c
islombek1221 <146804944+islombek1221@users.noreply.github.com>
2023-10-03 10:55:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// let number = +prompt('Sonni kiriting')
+
+// switch (number) {
+//     case (101):
+//         alert('пожарный');
+//         break;
+//     case (102):
+//         alert('полиция');
+//         break;
+//     case (103):
+//         alert('скорая');
+//         break;
+//     default:
+//         alert('Jinimisan');
+//         break;
+// }
+
+
+
+// let name = 'abcd';
+
+// switch (name) {
+//     case ('Islombek'):
+//         alert('welcome');
+//         break;
+//     case ('Islomjon'):
+//         alert('Kim bu');
+//         break;
+//     default:
+//         alert('Yoqol')
+//         break;
+// }
+
+
+let non = 0;
+
+while(non < 5){
+    console.log(`${non} non`);
+
+    non = non + 4;
+}
